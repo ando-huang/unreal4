21b37654cb23ff10fcfec9b3e6ea21a8c0185fcc
ando-huang <ahuang09182000@gmail.com>
2021-01-01 14:04:44 -0800

    start

--- hello/helloworld.cpp
+++ hello/helloworld.cpp
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <iostream>
+#include <ctime>
+
+void PlayGameAtDifficulty(int difficulty){
+    return;
+}
 
 int main(){
-    printf("Hello World\n");
+    int difficulty = 2;
+    int maxDifficulty = 10;
+
+    while(difficulty <= maxDifficulty){
+        PlayGameAtDifficulty(difficulty);
+        std::cin.clear();
+        std::cin.ignore();
+        difficulty++;
+    }
+    std::cout << "Good job!\n";
     return 0;
 }
---
+++ hello/triplex.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <ctime>
+
+void PlayGameAtDifficulty(int difficulty){
+    std::cout << "You're an agent trying to break into a level" << std::endl;
+}
