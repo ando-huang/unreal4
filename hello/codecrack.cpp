f05a7dc7fdac543d0387c7c3ee552132440872f0
ando-huang <ahuang09182000@gmail.com>
2021-01-03 23:48:36 -0800

    final

--- hello/codecrack.cpp
+++ hello/codecrack.cpp
@@ -2,11 +2,14 @@
 #include <iostream>
 #include <ctime>
 
-void PlayGameAtDifficulty(int difficulty){
-    printf("Welcome to level %d.\n", difficulty); //recall that C functions are fully functional in C++
-    const int A = 1 + rand() % (difficulty + 1);
-    const int B = 1 + rand() % (difficulty + 1);
-    const int C = 1 + rand() % (difficulty + 1);
+int PlayGameAtDifficulty(int difficulty){
+    printf("Welcome to level %d.\n", difficulty-1); //recall that C functions are fully functional in C++
+    int tries = 0;
+
+    srand(time(0)); //use this to reinitialize rand so each run is different
+    const int A = 1 + rand() % difficulty; //generates int between 1 and difficulty+1
+    const int B = 1 + rand() % difficulty; 
+    const int C = 1 + rand() % difficulty;
 
     const int Sum = A + B + C;
     const int Prod = A * B * C;
@@ -16,32 +19,34 @@ void PlayGameAtDifficulty(int difficulty){
     int GuessC = -1;
     int GSum = -1;
     int GProd = -1;
-    int correct = -1;
-    while(correct == -1){
+
+    while(GSum != Sum || GProd != Prod){
         printf("Sum: %d\nProduct: %d\n", Sum, Prod);
         printf("Enter your guesses for the variables, separated by a space.\n");
         scanf("%d %d %d", &GuessA, &GuessB, &GuessC);
         GSum = GuessA + GuessB + GuessC;
         GProd = GuessA * GuessB * GuessC;
-        if(GSum == Sum && GProd == Prod){
-            correct = 1;
-        }
+        tries++;
     }
+    
     printf("--- Code cracked ---\n");
-    return;
+    return tries;
 }
 
 int main(){
     int difficulty = 2;
     const int maxDifficulty = 10;
 
+    int tries = 0;
+    
     while(difficulty <= maxDifficulty){
-        PlayGameAtDifficulty(difficulty);
+        tries += PlayGameAtDifficulty(difficulty);
         //clears input for new line, extra space
         std::cin.clear();
         std::cin.ignore();
         difficulty++;
     }
-    std::cout << "Good job!\n";
+    std::cout << "Good job! You beat all the codes!\n";
+    printf("Your score was %0.2f.\n", float(maxDifficulty/tries));
     return 0;
 }
